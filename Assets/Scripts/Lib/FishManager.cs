a7ff2213c7c4d46ca8919e99951e73e2268a9f97
aratius <arata1129matsu@icloud.com>
2022-04-29 17:07:13 +0900

    FishDataをメンバに保存

--- Assets/Scripts/Lib/FishManager.cs
+++ Assets/Scripts/Lib/FishManager.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+using System.Collections.Generic;
 using LitJson;
 using Cysharp.Threading.Tasks;
 
@@ -9,12 +9,26 @@ using Cysharp.Threading.Tasks;
 public class FishManager
 {
 
+  private List<FishData> _dataList = new List<FishData>();
+
   public void Init()
   {
     this._UpdateFish();
   }
 
+  /// <summary>
+  /// 一匹要求されてどれを返すかのアルゴリズムをここで
+  /// </summary>
+  /// <returns></returns>
+  public FishData getOne()
+  {
+    return this._dataList[0];
+  }
 
+  /// <summary>
+  /// ポーリング実行
+  /// </summary>
+  /// <returns></returns>
   private async UniTask _UpdateFish()
   {
     JsonData data = await Api.GetList();
@@ -23,6 +37,16 @@ public class FishManager
     {
       JsonData fishJson = fishes[i];
       FishData fishData = new FishData(fishJson);
+
+      string id = fishData.id;
+      bool found = false;
+      for (int j = 0; j < this._dataList.Count; j++)
+      {
+        string otherId = this._dataList[j].id;
+        if (id == otherId) found = true;
+      }
+
+      if (!found) this._dataList.Add(fishData);
     }
   }
 }
